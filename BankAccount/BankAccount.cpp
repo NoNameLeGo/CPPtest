1c0263cd101117f9c0113c59a190594a08be9f5a
NoNameLeGo <nonamelego@qq.com>
2026-03-27 00:43:28 +0800

    在当前程序的基础上，增加功能，使得能记录最近一次的交易类型和金额。

--- BankAccount/BankAccount.cpp
+++ BankAccount/BankAccount.cpp
@@ -7,15 +7,21 @@ private:
 	string accountHolderName;
 	string accountNumber;
 	double balance;
+	string lastTransactionType;
+	double lastTransactionAmount;
 public:
 	BankAccount(string name, string number, double initialBalance) {
 		accountHolderName = name;
 		accountNumber = number;
 		balance = initialBalance;
+		lastTransactionType = "无";
+		lastTransactionAmount = 0;
 	}
 	void deposit(double amount) {
 		if (amount > 0) {
 			balance += amount;
+			lastTransactionType = "存款";
+			lastTransactionAmount = amount;
 			cout << "已存入: " << amount << endl;
 		} else {
 			cout << "存款金额无效" << endl;
@@ -24,8 +30,12 @@ public:
 	void withdraw(double amount) {
 		if (amount > 0 && amount <= balance) {
 			balance -= amount;
+			lastTransactionType = "取款";
+			lastTransactionAmount = amount;
 			cout << "已取款: " << amount << endl;
 		} else {
+			lastTransactionType = "失败";
+			lastTransactionAmount = amount;
 			cout << "提款金额无效或账户余额不足." << endl;
 		}
 	}
@@ -34,7 +44,16 @@ public:
 		cout << "户主: " << accountHolderName << endl;
 		cout << "账号: " << accountNumber << endl;
 		cout << "余额: " << balance << endl;
+		if (lastTransactionType != "无") {
+			cout << "最近交易: " << lastTransactionType << " " << lastTransactionAmount << endl;
+		}
 		cout << "-----------------------------" << endl;
+	}
+	string getLastTransactionType() const {
+		return lastTransactionType;
+	}
+	double getLastTransactionAmount() const {
+		return lastTransactionAmount;
 	}
 };
 
