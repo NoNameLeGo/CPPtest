479b5660feca9a18da3d4bc526d1585df9ca23d9
NoNameLeGo <nonamelego@qq.com>
2026-04-28 15:44:34 +0800

    抄了老师的派生，同时为了好玩在README中加上了Star History

---
+++ 20260428multiple_inheritance_test/20260428multiple_inheritance_test.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Teacher {
+protected:
+    string name;
+    int age;
+    string title;
+public:
+    Teacher(string nam, int a, string t) {
+        name = nam;
+        age = a;
+        title = t;
+    }
+    void display() {
+        cout << "name:" << name << "\t age:" << age << "\ttitle:" << title << endl;
+    }
+};
+
+class Student {
+protected:
+    string name1;
+    char sex;
+    float score;
+public:
+    Student(string na, char s, float sc) {
+        name1 = na;
+        sex = s;
+        score = sc;
+    }
+    void display1() {
+        cout << "name:" << name1 << "\t sex:" << sex << "\t score:" << score << endl;
+    }
+};
+
+class Graduate : public Teacher, public Student {
+private:
+    float wage;
+public:
+    Graduate(string nam, int a, string t, string na, char s, float sc, float w)
+        : Teacher(nam, a, t), Student(na, s, sc), wage(w) {
+    }
+    void show() {
+        cout << "name:" << name << "\t age:" << age << "\t sex:" << sex << "\t score:" << score << "\t title:" << title << "\t wage:" << wage << endl;
+    }
+};
+
+int main() {
+    Graduate gra1("张丽", 21, "讲师", "张丽", 'F', 89.5, 1200);
+    gra1.show();
+    return 0;
+}
--- README.md
+++ README.md
@@ -4,3 +4,13 @@
 放个石墩子在这试试
 
 ![Stone Badge](https://stone.professorlee.work/api/stone/NoNameLeGo/CPPtest)
+
+## Star History
+
+<a href="https://www.star-history.com/?repos=NoNameLeGo%2FCPPtest&type=date&legend=top-left">
+ <picture>
+   <source media="(prefers-color-scheme: dark)" srcset="https://api.star-history.com/chart?repos=NoNameLeGo/CPPtest&type=date&theme=dark&legend=bottom-right" />
+   <source media="(prefers-color-scheme: light)" srcset="https://api.star-history.com/chart?repos=NoNameLeGo/CPPtest&type=date&legend=bottom-right" />
+   <img alt="Star History Chart" src="https://api.star-history.com/chart?repos=NoNameLeGo/CPPtest&type=date&legend=bottom-right" />
+ </picture>
+</a>
